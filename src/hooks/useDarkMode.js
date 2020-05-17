571b5961b8d345d297df3c424ec3b8acd10ee13e
Michael Sanchez <michaelsanchez42@live.com>
2020-05-16 18:31:59 -0600

    mvp

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import useLocalStorage from "./useLocalStorage"
+
+export const useDarkMode = (initialValue) =>{
+    const [darkMode, setDarkMode] = useLocalStorage("dark-mode", initialValue);
+    if (darkMode){
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+    useEffect (()=>{
+      if (darkMode){
+        document.body.classList.add('dark-mode');
+      } else {
+        document.body.classList.remove('dark-mode');
+      }
+    }, [darkMode]);
+
+    return [darkMode, setDarkMode];
+  }
+
+export default useDarkMode;
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+// import ReactDOM from "react-dom";
+// import axios from "axios";
+
+
+
+export const useLocalStorage = (key, initialValue) => {
+ 
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }
+
+  return [storedValue, setValue];
+
+};
+
+export default useLocalStorage;
+
+
